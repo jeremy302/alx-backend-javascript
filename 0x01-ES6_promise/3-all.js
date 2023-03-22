1a737a0827cbf44466fec936740329e7194cb1c5
jeremy302 <olajeremy123@gmail.com>
2023-03-22 18:53:25 +0100

    patch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, name]) => console.log(`${photo.body}, ${name.firstName}, ${name.lastName}`))
+    .then(([photo, name]) => { console.log(`${photo.body}, ${name.firstName}, ${name.lastName}`); })
     .catch(() => console.log('Signup system offline'));
 }
