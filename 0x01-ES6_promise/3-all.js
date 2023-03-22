7043b6de76ed6a5e24ad13336b968fc09f86847b
jeremy302 <olajeremy123@gmail.com>
2023-03-22 21:20:52 +0100

    removed comma

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => { console.log(`${res[0].body}, ${res[1].firstName}, ${res[1].lastName}`); })
+    .then(([p, u]) => { console.log(`${p.body} ${u.firstName} ${u.lastName}`); })
     .catch(() => console.log('Signup system offline'));
 }
