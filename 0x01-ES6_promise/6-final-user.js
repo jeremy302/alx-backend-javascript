e66fc4d9577db3ef9e4a9f3c2bbb25f15e917909
jeremy302 <olajeremy123@gmail.com>
2023-03-22 21:45:43 +0100

    patch

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled(
     [signUpUser(firstName, lastName), uploadPhoto(filename)],
   ).then((res) => res.map(
-    ({ status, value, reason }) => ({ status, value: value ?? String(reason) }),
+    ({ status, value, reason }) => ({ status, value: value || String(reason) }),
   ));
 }
