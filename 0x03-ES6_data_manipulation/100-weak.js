a89557be4ab1915ad2319ad992faf155a6e9ceaa
jeremy302 <olajeremy123@gmail.com>
2023-03-28 01:02:44 +0100

    patch

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,8 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
   if (count >= 5) throw Error('Endpoint load is high');
-  weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, count);
 }
