2edefa83119641a66c46557f4275237138ffabfe
jeremy302 <olajeremy123@gmail.com>
2023-05-18 11:03:05 +0100

    patch

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,10 +6,10 @@ describe('0-calcul.js', function(){
     it('should round properly', function(){
         assert.ok(calculateNumber(1, 1) === 2);
         assert.ok(calculateNumber(1, -1) === 0);
-        // assert(calculateNumber(1.0, 1.0) === 2);
-        // assert(calculateNumber(1.2, 1.2) === 2);
-        // assert(calculateNumber(1.4, 1.4) === 2);
-        // assert(calculateNumber(1.5, 1.4999) === 3);
-        // assert(calculateNumber(-1.4, -1.4999) === -2);
+        assert.ok(calculateNumber(1.0, 1.0) === 2);
+        assert.ok(calculateNumber(1.2, 1.2) === 2);
+        assert.ok(calculateNumber(1.4, 1.4) === 2);
+        assert.ok(calculateNumber(1.5, 1.4999) === 3);
+        assert.ok(calculateNumber(-1.4, -1.4999) === -2);
     });
 });
