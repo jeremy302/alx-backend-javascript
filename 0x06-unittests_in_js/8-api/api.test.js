9fb0d6aa70b7344d5be1f8732edf9ce210fdd6c1
jeremy302 <olajeremy123@gmail.com>
2023-05-18 12:39:44 +0100

    patch

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,9 +4,7 @@ const request = require('request');
 
 describe('express server', function(){
     it('status and body', function(done){
-        const url = 'https://localhost:7865/';
-        
-        request.get(url,(err, res, body)=>{
+        request.get('http://localhost:7865/',(err, res, body)=>{
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal('Welcome to the payment system');
             done();
