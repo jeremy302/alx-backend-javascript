c061dd1ee6c2d6a13b83c2be3d33ac548dd636f4
jeremy302 <olajeremy123@gmail.com>
2023-05-18 10:53:56 +0100

    test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,6 @@
-module.exports =  function calculateNumber(a, b){
-    return Math.round(a) + Math.round(b);
-}
+// module.exports =  function calculateNumber(a, b){
+//     return Math.round(a) + Math.round(b);
+// }
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
