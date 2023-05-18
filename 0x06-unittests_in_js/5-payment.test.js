52ce4bfcbacd0c9ac0de2122340e7f8b1a731730
jeremy302 <olajeremy123@gmail.com>
2023-05-18 12:03:52 +0100

    patch

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -6,25 +6,27 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 
 describe('sendPaymentRequestToApi', function(){
-    const sandbox = sinon.createSandbox();
+    // const sandbox = sinon.createSandbox();
     // let stub = null;
+    let spy = null;
     beforeEach(function(){
-        sandbox.spy(console);
+        // sandbox.spy(console);
+        spy = spy || sinon.spy(console);
         // stub = sandbox.stub(Utils, "calculateNumber");
     });
     afterEach(function(){
-        sandbox.restore();
+        spy.log.resetHistory();
     });
     it('sendPaymentRequestToAPI(100, 20)', function(){
-        expect(console.log.callCount).to.be.equal(0);
+        expect(spy.log.callCount).to.be.equal(0);
         sendPaymentRequestToApi(100, 20);
-        expect(console.log.callCount).to.be.equal(1);
-        expect(console.log.calledWith('The total is: 120')).to.equal(true);
+        expect(spy.log.callCount).to.be.equal(1);
+        expect(spy.log.calledWith('The total is: 120')).to.equal(true);
     });
     it('sendPaymentRequestToAPI(10, 10)', function(){
-        expect(console.log.callCount).to.be.equal(0);
+        expect(spy.log.callCount).to.be.equal(0);
         sendPaymentRequestToApi(10, 10);
-        expect(console.log.callCount).to.be.equal(1);
-        expect(console.log.calledWith('The total is: 20')).to.equal(true);
+        expect(spy.log.callCount).to.be.equal(1);
+        expect(spy.log.calledWith('The total is: 20')).to.equal(true);
     });
 });
